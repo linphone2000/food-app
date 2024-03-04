155c89c1a88f41478b530c6096ed9df044f603d3
Lin Phone Myint Zaw <linphonem@gmail.com>
2024-03-04 19:56:24 +0630

    Add AKMH admin panel

---
+++ admin/Header_Components/navbar.php
@@ -0,0 +1,38 @@
+ <nav class="sb-topnav navbar navbar-expand navbar-dark ">
+            <!-- Navbar Brand-->
+             <a class="navbar-brand ps-3" href="#home">
+                 <img src="assets/Chop_Chop_Logo_Pink.png" class="navbar-logo" />
+             </a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav  navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#Home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Menu">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Contact">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user fa-fw"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-right class-for-dropdown">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Register</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
